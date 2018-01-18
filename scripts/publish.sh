31ffb23e4c701176750149f1123fad792ce3f193
Sandy Maguire <sandy@sandymaguire.me>
2018-01-18 14:19:38 -0700

    improve publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,8 +10,9 @@ loc="site/posts"
 
 WIP="wip/${wipslug}.markdown"
 SLUG="${loc}/$(date +'%Y-%m-%d')-${slug}.markdown"
-git mv $WIP $SLUG
+git mv $WIP $SLUG || mv $WIP $SLUG
 sed -i "s/TO_BE_DETERMINED/$(date +'%Y-%m-%d %H:%M')/" $SLUG
+git add $SLUG
 git commit -m "published ${slug}"
 
 echo "Published as $SLUG"
