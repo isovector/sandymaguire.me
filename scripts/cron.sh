82a6440562a90385771285c1f4a780470b9062c9
Sandy Maguire <sandy@sandymaguire.me>
2019-03-15 14:44:37 -0400

    update with book reviews

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/usr/bin/zsh
 
+source /home/sandy/.zshrc
+
+set -x
 cd /home/sandy/prj/arbtt-graph/
 ./update
+cd /home/sandy/prj/sandymaguire.me
+rm -rf dist/dailystats
+cp -R ../arbtt-graph/render dist/dailystats
+rsync -r -a -v -e "ssh -i $HOME/.ssh/santino.pem" dist/ "ubuntu@$SERENADE:/data/blog/_live/we-can-solve-this/"
 
