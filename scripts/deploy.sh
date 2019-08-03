63712884b289d41343ff6c71ce054a9842e58196
Sandy Maguire <sandy@sandymaguire.me>
2019-08-03 11:18:06 -0400

    YES

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+set +xe
+
 echo "deploying on server..."
 stack build
 stack exec wcst
-cp -Rv ../arbtt-graph/render dist/dailystats
-rsync -r -a -v -e "ssh -i $HOME/.ssh/santino.pem" dist/ "ubuntu@$SERENADE:/data/blog/_live/we-can-solve-this/"
+rm -rf docs/
+mkdir docs
+mv dist/* docs/
+git add docs
+git commit -m "i am excellent at programming"
+git push
 
