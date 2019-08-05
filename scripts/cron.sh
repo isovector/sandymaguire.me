d725a676fcba41bf7573dae3ef37a703799883b6
Sandy Maguire <sandy@sandymaguire.me>
2019-08-05 12:15:58 -0400

    update cron

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -6,7 +6,9 @@ set -x
 cd /home/sandy/prj/arbtt-graph/
 ./update
 cd /home/sandy/prj/sandymaguire.me
-rm -rf dist/dailystats
-cp -R ../arbtt-graph/render dist/dailystats
-rsync -r -a -v -e "ssh -i $HOME/.ssh/santino.pem" dist/ "ubuntu@$SERENADE:/data/blog/_live/we-can-solve-this/"
+rm -rf docs/dailystats
+cp -R ../arbtt-graph/render docs/dailystats
+git add docs/dailystats
+git commit "update daily stats"
+git push
 
