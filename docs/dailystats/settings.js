a828a415e54a90a5d880b03f2dc50b03e36e6488
Sandy Maguire <sandy@sandymaguire.me>
2020-01-26 16:15:56 +0700

    update daily stats

--- docs/dailystats/settings.js
+++ docs/dailystats/settings.js
@@ -1,6 +1,6 @@
 // Barcode view settings
 var display_groups = [];
 // Productive
-display_groups.push(["prj", "papers", "Pentathlon", "music"]);
+display_groups.push(["prj", "papers", "Pentathlon", "music", "security"]);
 display_groups.push(["slack", "comm", "call"]);
-display_groups.push(["web", "starcraft", "pp"]);
+display_groups.push(["web", "starcraft", "pp", "tv"]);
